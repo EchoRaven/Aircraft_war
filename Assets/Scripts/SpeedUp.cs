6a7da0fa9ac6e38ca21f65a1f2ddff70d4571ccc
Cishoon <1329115097@qq.com>
2022-03-30 09:53:54 +0800

    作弊码+按c加速

--- Assets/Scripts/SpeedUp.cs
+++ Assets/Scripts/SpeedUp.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class SpeedUp : MonoBehaviour
 {
+    private PlayerStatement player;
+    private int speed_timer;
+    private void Start()
+    {
+        player = GetComponentInChildren<PlayerStatement>();
+    }
+    
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.C)){
-            print("You print C");
+            if(player.speed == 200){
+                speed_timer = 100;
+                player.speed = 1000;
+            }
+        }
+        speed_timer--;
+        if(speed_timer <= 0){
+            player.speed = 200;
         }
     }
 }
